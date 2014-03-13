7eb17944560ebb492f4aea81ff88fb9a3b1036b4
js2854 <js2854@163.com>
2014-03-13 20:02:10 +0800

    1.重复下载同一文件时支持重命名，文件名自动编号； 2.支持调试(重启FDM生效

--- FdmRenamePlugin.cpp
+++ FdmRenamePlugin.cpp
@@ -5,13 +5,56 @@
 #include "./include/fdm.h"
 #include <stdio.h>
 #include <io.h>		// for _access
+#include <time.h>	// for time
 
 #include "ChineseConvert.h"
+
+#define DLL_NAME		"FdmRenamePlugin.dll"
+#define DEBUG_FILE		"FdmRenamePluginDebug"
+#define MAX_TRY			10
+
+#define FILE_EXIST(a)	(_access(a, 0) != -1)
 
 static bool g_exit = false;
 static HANDLE g_hThread = NULL;
 
+//debug and log
+static FILE* g_fp = NULL;
+
 void KillThread();
+
+void DebugCheck()
+{
+	char dllDir[MAX_PATH] = {0};	
+	GetModuleFileName(GetModuleHandle(DLL_NAME), dllDir, MAX_PATH);
+	char* ptr = strrchr(dllDir, '\\');
+	ptr[1] = '\0';
+
+	string debugFilePath = string(dllDir) + DEBUG_FILE;
+	if (FILE_EXIST(debugFilePath.c_str()))
+	{
+		string logFilePath = string(dllDir) + DLL_NAME + ".log";
+		g_fp = fopen(logFilePath.c_str(), "a+");
+	}
+}
+
+void Log2File(const char* format_str, ...)
+{
+	if (g_fp)
+	{
+		time_t t = time(NULL);
+		struct tm *ptime = localtime(&t);
+		va_list p_list;
+		va_start(p_list, format_str);
+		fprintf(g_fp, "[%04d%02d%02d-%02d:%02d:%02d]", 
+			(1900+ptime->tm_year), (1+ptime->tm_mon), ptime->tm_mday,
+            ptime->tm_hour, ptime->tm_min, ptime->tm_sec);
+		vfprintf(g_fp, format_str, p_list);
+		va_end(p_list);
+
+		fflush(g_fp);
+	}
+}
 
 BOOL APIENTRY DllMain( HANDLE hModule, 
                        DWORD  ul_reason_for_call, 
@@ -21,6 +64,7 @@ BOOL APIENTRY DllMain( HANDLE hModule,
     switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
+		DebugCheck();
 		break;
 	case DLL_THREAD_ATTACH:
 		break;
@@ -54,8 +98,40 @@ void FdmApiPluginGetDescription (LPSTR pszPluginShortName, LPSTR pszPluginLongNa
 {
 	strcpy (pszPluginShortName, "FDMExPlug");
 	strcpy (pszPluginLongName, "FDM Rename plugin");
-	strcpy (pszPluginVersion, "1.0");
-	strcpy (pszPluginDescription, "This plugin helps to adjust Chinese filename.");
+	strcpy (pszPluginVersion, "1.0.0.3");
+	strcpy (pszPluginDescription, "This plugin helps to adjust UTF-8 Chinese filename.");
+}
+// if `dst` file exist, src file will be rename to format like `dst (1)`  
+string RenameEx(const char* src, const char* dst)
+{
+	string strNewFilePath = dst;
+	if (FILE_EXIST(dst))
+	{
+		char tmpPath[MAX_PATH] = {0};
+		const char* pExt = strrchr(dst, '.');
+		string filename = string(dst).substr(0, pExt - dst);
+		for (int i = 1; i <= MAX_TRY; i++)
+		{
+			sprintf(tmpPath, "%s (%d)%s", filename.c_str(), i, pExt);
+			if (!FILE_EXIST(tmpPath))
+			{
+				strNewFilePath = tmpPath;
+				break;
+			}
+		}
+	}
+	
+	if (rename(src, strNewFilePath.c_str()) != 0)
+	{
+		strNewFilePath = "";
+		Log2File("Rename '%s' -> '%s' failure, Error: %s\n", src, strNewFilePath.c_str(), strerror(errno));
+	}
+	else
+	{
+		Log2File("Rename '%s' -> '%s' success.\n", src, strNewFilePath.c_str());
+	}
+
+	return strNewFilePath;//return real file name
 }
 
 // UTF-8 To GB2312
@@ -73,12 +149,12 @@ void RenameFile()
 		dld->getFileName(szFilePath);
 		UINT nStatCode = dld->getState();
 		
-		bool bFileExist = (_access(szFilePath, 0) != -1);
+		bool bFileExist = FILE_EXIST(szFilePath);
 		bool bIsUtf8Filename = (strchr(szFilePath, '%') != NULL || CChineseConvert::IsUTF8String(szFilePath));
 		if (nStatCode == FADS_FINISHED && bFileExist && bIsUtf8Filename)
 		{// Download finished and file exist and exist '%' char in filename
 			string strNewFilename = CChineseConvert::Url_Utf8ToGB2312(szFilePath);
-			rename(szFilePath, strNewFilename.c_str());
+			RenameEx(szFilePath, strNewFilename.c_str());			
 		}
 	}
 }
@@ -90,7 +166,14 @@ void KillThread()
 	{
 		TerminateThread(g_hThread,0);
 		CloseHandle(g_hThread);
-	}	
+		g_hThread = NULL;
+	}
+	
+	if (NULL != g_fp)
+	{
+		fclose(g_fp);
+		g_fp = NULL;
+	}
 }
 
 DWORD WINAPI _threadJob (LPVOID)
